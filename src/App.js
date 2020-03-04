a7bdc7f8a7fa08aa2f6d544ba64557336f94bcdf
josh <junk@lets-roll.de>
2020-03-04 09:03:50 +0100

    first

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+import HomePage from './homepage.component';
+
+import './homepage.styles.scss';
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
